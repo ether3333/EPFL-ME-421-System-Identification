794a2e81a62caa3419caa95344743342b4ea01d9
Iseo Shin <ether@kaist.ac.kr>
2026-04-27 14:16:06 +0200

    Update CE2.m

--- CE2/CE2.m
+++ CE2/CE2.m
@@ -1,6 +1,6 @@
 clearvars -except u y
 
-%%2.1.1
+%% 2.1.1 FIR model identification
 [y, u, Ts] = GetExperimentData('data.m');
 
 % u, y are column vectors
@@ -8,47 +8,43 @@ u = u(:);
 y = y(:);
 
 % FIR model settings
-m = 200;   % Number of FIR parameters
-d = 1;     % Input delay
+m = 200;
+d = 1;
 
 % Basic size check
 N = length(u);
 if length(y) ~= N
     error('u and y must have the same length.');
 end
-y_used = y(m+1:N);
 
 %Make each row to 200 inputs
 first_col = u(m:N-1);
 first_row = u(m:-1:1);
 Phi = toeplitz(first_col, first_row);
 
-%define Theta hat
-theta_hat = Phi \ y_used;
-
-% Compute the predicted output
+y_used = y(m+1:N); % follow conditions of FIR
+%1
+theta_hat = Phi \ y_used; % Least-square solution
+%2
 y_hat = Phi * theta_hat;
-
+prediction_error = y_used - y_hat;
+J = sum(prediction_error.^2);
 % Plot measured output and predicted output
 figure;
+plot(k, y_used, 'b', 'LineWidth', 1.2);
+hold on;
+plot(k, y_hat, 'r--', 'LineWidth', 1.2);
+hold off;
 
-prediction_error = y_used - y_hat;
-% Compute the loss function
-J = sum(prediction_error.^2);
-
+xlabel('k');
+ylabel('Output');
+title('Measured output and predicted output');
+legend('Measured output y(k)', 'Predicted output \ity\rm_hat(k,\theta_hat)', 'Location', 'best');
+grid on;
 % Display the loss function
 fprintf('Loss function J(theta_hat) = %.6f\n', J);
 
+%3
 % Estimate noise variance
-n = length(y_used);      % Number of output samples used
-p = m;                   % Number of estimated parameters
-sigma2_hat = J / (n - p);
-
-% Compute covariance matrix of estimated parameters
-Cov_theta = sigma2_hat * inv(Phi' * Phi);
-
-sigma_theta = sqrt(diag(Cov_theta));
-
-errorbar(1:m, theta_hat, 2*sigma_theta, 'o');
-
-figure;
+sigma_sq_hat = J / ((N-m)-m); %N-m is #of data actually used
+cov_theta_hat = sigma_sq_hat * inv(Phi'*Phi);
